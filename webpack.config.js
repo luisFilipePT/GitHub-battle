575d4b7b7e39410df11e03984260c4674c83320b
Luis <luis.fgbo@gmail.com>
2017-08-13 15:23:49 +0100

    build flow to production added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,8 +11,8 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.(js)$/, use: 'babel-loader'},
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader']},
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         ]
     },
     devServer: {
@@ -21,3 +22,19 @@ module.exports = {
         template: 'app/index.html'
     })]
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    )
+}
+
+// Not implemented deploy to firebase
+// If needed check https://learn.tylermcginnis.com/courses/50507/lectures/2466854#/finished
+
+module.exports = config;
